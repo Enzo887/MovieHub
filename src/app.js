906e6362cf4ce29607183ef52c8cecac0e15d912
Enzo887 <enzoduarte2014@gmail.com>
2026-01-06 12:50:28 -0300

    feature y chore: continue la vista principal. Servi la carpeta public, poniendo dentro de ella bootstrap

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT ?? 3000
 app.set('view engine', 'ejs')
 
 app.use('/', routerMovie)
+app.use(express.static('public'))
 
 app.listen(PORT, ()=>{ 
     console.log(`Escuchando en http://localhost:${PORT}`)
