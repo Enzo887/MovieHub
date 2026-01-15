b839a1157b7be8976d6e664b6843cac7f6d3db57
Enzo887 <enzoduarte2014@gmail.com>
2026-01-15 09:49:02 -0300

    feat: agregue una vista de TMDB para ver su funcionamiento

--- public/js/ui/render.js
+++ public/js/ui/render.js
@@ -6,10 +6,11 @@ export function renderPelis(movies){
         contenedorPelis.appendChild(crearCardPelicula(movie))
     });
 }
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 
 function crearCardPelicula(movie){
     const img = document.createElement('img')
-    img.src = movie.poster
+    img.src = IMAGE_BASE_URL + movie.poster_path
     img.alt = movie.title
     img.className = 'netflix-card rounded'
 
